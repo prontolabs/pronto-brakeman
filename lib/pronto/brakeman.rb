587a6350b3958534c7dfde866a9321d5a1030244
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2013-10-08 22:09:56 +0300

    Replace usage of #select#first with #find

--- lib/pronto/brakeman.rb
+++ lib/pronto/brakeman.rb
@@ -15,39 +15,35 @@ module Pronto
         output = ::Brakeman.run(app_path: '.',
                                 output_formats: [:to_s],
                                 only_files: files)
-        messages_for(ruby_patches, output)
+        messages_for(ruby_patches, output).compact
       else
         []
       end
     end
 
     def messages_for(ruby_patches, output)
-      result = []
-
       output.checks.all_warnings.each do |warning|
         patch = patch_for_warning(ruby_patches, warning)
 
         if patch
-          line = patch.added_lines.select do |added_line|
+          line = patch.added_lines.find do |added_line|
             added_line.new_lineno == warning.line
-          end.first
+          end
 
-          result << new_warning_message(line, warning) if line
+          new_message(line, warning) if line
         end
       end
-
-      result
     end
 
-    def new_warning_message(line, warning)
+    def new_message(line, warning)
       Message.new(line.patch.delta.new_file[:path], line, :warning,
                   "Possible security vulnerability: #{warning.message}")
     end
 
     def patch_for_warning(ruby_patches, warning)
-      ruby_patches.select do |patch|
+      ruby_patches.find do |patch|
         patch.new_file_full_path.to_s == warning.file
-      end.first
+      end
     end
   end
 end
