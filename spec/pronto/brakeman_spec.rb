90d9fab8647640938424d325f3cc29b9a373e76d
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2015-04-25 22:20:22 +0300

    Add an integration spec

--- spec/pronto/brakeman_spec.rb
+++ spec/pronto/brakeman_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require 'ostruct'
 
 module Pronto
   describe Brakeman do
@@ -16,6 +15,17 @@ module Pronto
       context 'no patches' do
         let(:patches) { [] }
         it { should == [] }
+      end
+
+      context 'patches with a single unsafe redirect' do
+        let(:repo) { Git::Repository.new('spec/fixtures/test.git') }
+        let(:patches) { repo.diff('da70127') }
+
+        its(:count) { should == 1 }
+        its(:'first.msg') do
+          should ==
+            'Possible security vulnerability: Possible unprotected redirect'
+        end
       end
     end
   end
