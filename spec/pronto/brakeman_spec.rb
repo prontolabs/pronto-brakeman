c66abf445408718e197919ebe254654e0e1838bd
Daniel Holz <dgholz@gmail.com>
2020-05-28 16:10:36 +0100

    Use single quotes for non-interpolated strings

--- spec/pronto/brakeman_spec.rb
+++ spec/pronto/brakeman_spec.rb
@@ -35,24 +35,24 @@ module Pronto
       end
     end
 
-    describe "#severity_for_confidence" do
+    describe '#severity_for_confidence' do
       subject { brakeman.severity_for_confidence(confidence_level) }
 
       let(:patches) { nil }
 
-      context "when confidence is HIGH" do
+      context 'when confidence is HIGH' do
         let(:confidence_level) { 0 }
 
         it { should == :fatal }
       end
 
-      context "when confidence is MEDIUM" do
+      context 'when confidence is MEDIUM' do
         let(:confidence_level) { 1 }
 
         it { should == :warning }
       end
 
-      context "when confidence is anything else" do
+      context 'when confidence is anything else' do
         let(:confidence_level) { 2 }
 
         it { should == :info }
