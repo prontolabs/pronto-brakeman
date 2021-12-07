cb8462057ce5cfc59cb86b567119d4161276b207
Noah Berman <noah-berman@cookpad.com>
2021-12-07 13:24:46 +0000

    Try show commit?

--- spec/pronto/brakeman_spec.rb
+++ spec/pronto/brakeman_spec.rb
@@ -45,7 +45,7 @@ module Pronto
       context 'with run all checks disabled' do
         let(:config_hash) { { 'brakeman' => { 'run_all_checks' => false } } }
         include_context 'test repo'
-        let(:patches) { repo.diff('7835d50') }
+        let(:patches) { repo.show_commit('7835d50') }
 
         it 'should disable all checks' do
           expect(brakeman.run_all_checks?).to eq false
@@ -57,7 +57,7 @@ module Pronto
       context 'with run all checks enabled' do
         let(:config_hash) { { 'brakeman' => { 'run_all_checks' => true } } }
         include_context 'test repo'
-        let(:patches) { repo.diff('7835d50') }
+        let(:patches) { repo.show_commit('7835d50') }
 
         it 'should enable all checks' do
           expect(brakeman.run_all_checks?).to eq true
