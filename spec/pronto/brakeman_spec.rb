ad923f013d49c151d02cb83c661632e17705e7c1
Noah Berman <noah-berman@cookpad.com>
2021-12-07 15:20:46 +0000

    Add test link_to that is vulnerable to reverse tabnabbing

--- spec/pronto/brakeman_spec.rb
+++ spec/pronto/brakeman_spec.rb
@@ -45,7 +45,7 @@ module Pronto
       context 'with run all checks disabled' do
         let(:config_hash) { { 'brakeman' => { 'run_all_checks' => false } } }
         include_context 'test repo'
-        let(:patches) { repo.diff('da70127') }
+        let(:patches) { repo.diff('HEAD') }
 
         it 'should disable all checks' do
           expect(brakeman.run_all_checks?).to eq false
@@ -57,7 +57,7 @@ module Pronto
       context 'with run all checks enabled' do
         let(:config_hash) { { 'brakeman' => { 'run_all_checks' => true } } }
         include_context 'test repo'
-        let(:patches) { repo.diff('da70127') }
+        let(:patches) { repo.diff('HEAD') }
 
         it 'should enable all checks' do
           expect(brakeman.run_all_checks?).to eq true
